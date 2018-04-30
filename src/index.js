c03f4a7c9151e8281fb98529c3c6c41661d77d29
boushi-bird <boushi-bird@users.noreply.github.com>
2018-04-30 20:15:12 +0900

    データを読み込めない場合は表示しないように修正

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ const scriptId = 'VsD8NEfx'
 const listFrame = new ListFrame(document)
 
 const run = () => {
-  listFrame.show()
-
   if (!global.member_card_fire_date ||
     !global.member_data || !global.member_data['CARD'] ||
     !global.base_data) {
-    global.alert('データを読み込み中または未ログイン、あるいはデータリストのページではありません。')
+    global.alert(`[三国志大戦 解任ブックマークレット]
+データを読み込み中またはデータリストのページではありません。`)
+    listFrame.hide()
     return
   }
+  listFrame.show()
+
   const searcher = new CardSearcher(
     global.base_data,
     global.member_data['CARD'])
