52a03ba72e415ffb2008e2fbba3c78b7f2ef2c93
boushi-bird <boushi-bird@users.noreply.github.com>
2019-03-24 18:32:59 +0900

    npm run build

--- docs/bookmarklet/local.js
+++ docs/bookmarklet/local.js
@@ -1,1 +1,1 @@
-javascript:(function(t%2Ce%2Cn%2Cd%2Ci)%7Bif(t%5Bd%5D)return%20t%5Bd%5D()%3B(i%3De.getElementById(d))%7C%7C((i%3De.createElement(%22script%22)).src%3Dn%2Ci.id%3Dd%2Ce.body.appendChild(i))%7D)(window%2Cdocument%2C%22http%3A%2F%2Flocalhost%3A8080%2Fmain.js%22%2C%22VsD8NEfx%22)%3B
+javascript:(function(e%2Ct%2Cn%2Cd%2Cc)%7Bif(e%5Bd%5D)return%20e%5Bd%5D()%3B(c%3Dt.getElementById(d))%7C%7C((c%3Dt.createElement(%22script%22)).src%3Dn%2Cc.id%3Dd%2Ct.body.appendChild(c))%7D)(window%2Cdocument%2C%22http%3A%2F%2Flocalhost%3A8080%2Fbundle.js%22%2C%22VsD8NEfx%22)%3B
